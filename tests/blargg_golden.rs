c2e1ba1eda2786ff296d04fd3f325270cca64ee0
Pietro Lorefice <pietro@develer.com>
2019-02-06 13:48:43 +0100

    tests: reduce test tick durations

--- tests/blargg_golden.rs
+++ tests/blargg_golden.rs
@@ -5,11 +5,11 @@ use common::RomTest;
 #[test]
 fn passes_blargg_cpu_instrs() {
     RomTest::new(include_bytes!("../roms/blargg/cpu_instrs.gb"))
-        .must_run_and_match(261_175_572u64, include_bytes!("blargg/cpu_instrs.bin"));
+        .must_run_and_match(225_000_000u64, include_bytes!("blargg/cpu_instrs.bin"));
 }
 
 #[test]
 fn passes_blargg_instr_timing() {
     RomTest::new(include_bytes!("../roms/blargg/instr_timing.gb"))
-        .must_run_and_match(29_566_032u64, include_bytes!("blargg/instr_timing.bin"));
+        .must_run_and_match(3_000_000u64, include_bytes!("blargg/instr_timing.bin"));
 }
--- tests/gekkio_golden.rs
+++ tests/gekkio_golden.rs
@@ -12,7 +12,7 @@ fn passes_gekkio_acceptance_boot_div() {
         "../roms/gekkio/acceptance/boot_div-dmgABCmgb.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/boot_div-dmgABCmgb.bin"),
     );
 }
@@ -23,7 +23,7 @@ fn passes_gekkio_acceptance_boot_regs() {
         "../roms/gekkio/acceptance/boot_regs-dmgABC.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/boot_regs-dmgABC.bin"),
     );
 }
@@ -34,7 +34,7 @@ fn passes_gekkio_acceptance_boot_hwio() {
         "../roms/gekkio/acceptance/boot_hwio-dmgABCmgb.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/boot_hwio-dmgABCmgb.bin"),
     );
 }
@@ -42,7 +42,7 @@ fn passes_gekkio_acceptance_boot_hwio() {
 #[test]
 fn passes_gekkio_acceptance_div_timing() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/div_timing.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/div_timing.bin"),
     );
 }
@@ -54,7 +54,7 @@ fn passes_gekkio_acceptance_div_timing() {
 #[test]
 fn passes_gekkio_acceptance_bits_mem_oam() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/bits/mem_oam.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/bits/mem_oam.bin"),
     );
 }
@@ -62,7 +62,7 @@ fn passes_gekkio_acceptance_bits_mem_oam() {
 #[test]
 fn passes_gekkio_acceptance_bits_reg_f() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/bits/reg_f.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/bits/reg_f.bin"),
     );
 }
@@ -73,7 +73,7 @@ fn passes_gekkio_acceptance_bits_unused_hwio() {
         "../roms/gekkio/acceptance/bits/unused_hwio-GS.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/bits/unused_hwio-GS.bin"),
     );
 }
@@ -85,7 +85,7 @@ fn passes_gekkio_acceptance_bits_unused_hwio() {
 #[test]
 fn passes_gekkio_acceptance_instr_daa() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/instr/daa.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/instr/daa.bin"),
     );
 }
@@ -100,7 +100,7 @@ fn passes_gekkio_acceptance_timer_div_write() {
         "../roms/gekkio/acceptance/timer/div_write.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/div_write.bin"),
     );
 }
@@ -111,7 +111,7 @@ fn passes_gekkio_acceptance_timer_rapid_toggle() {
         "../roms/gekkio/acceptance/timer/rapid_toggle.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/rapid_toggle.bin"),
     );
 }
@@ -119,7 +119,7 @@ fn passes_gekkio_acceptance_timer_rapid_toggle() {
 #[test]
 fn passes_gekkio_acceptance_timer_tim00() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/timer/tim00.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim00.bin"),
     );
 }
@@ -130,7 +130,7 @@ fn passes_gekkio_acceptance_timer_tim00_div_trigger() {
         "../roms/gekkio/acceptance/timer/tim00_div_trigger.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim00_div_trigger.bin"),
     );
 }
@@ -138,7 +138,7 @@ fn passes_gekkio_acceptance_timer_tim00_div_trigger() {
 #[test]
 fn passes_gekkio_acceptance_timer_tim01() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/timer/tim01.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim01.bin"),
     );
 }
@@ -149,7 +149,7 @@ fn passes_gekkio_acceptance_timer_tim01_div_trigger() {
         "../roms/gekkio/acceptance/timer/tim01_div_trigger.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim01_div_trigger.bin"),
     );
 }
@@ -157,7 +157,7 @@ fn passes_gekkio_acceptance_timer_tim01_div_trigger() {
 #[test]
 fn passes_gekkio_acceptance_timer_tim10() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/timer/tim10.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim10.bin"),
     );
 }
@@ -168,7 +168,7 @@ fn passes_gekkio_acceptance_timer_tim10_div_trigger() {
         "../roms/gekkio/acceptance/timer/tim10_div_trigger.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim10_div_trigger.bin"),
     );
 }
@@ -176,7 +176,7 @@ fn passes_gekkio_acceptance_timer_tim10_div_trigger() {
 #[test]
 fn passes_gekkio_acceptance_timer_tim11() {
     RomTest::new(include_bytes!("../roms/gekkio/acceptance/timer/tim11.gb")).must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim11.bin"),
     );
 }
@@ -187,7 +187,7 @@ fn passes_gekkio_acceptance_timer_tim11_div_trigger() {
         "../roms/gekkio/acceptance/timer/tim11_div_trigger.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tim11_div_trigger.bin"),
     );
 }
@@ -198,7 +198,7 @@ fn passes_gekkio_acceptance_timer_tima_reload() {
         "../roms/gekkio/acceptance/timer/tima_reload.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tima_reload.bin"),
     );
 }
@@ -209,7 +209,7 @@ fn passes_gekkio_acceptance_timer_tima_write_reloading() {
         "../roms/gekkio/acceptance/timer/tima_write_reloading.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tima_write_reloading.bin"),
     );
 }
@@ -220,7 +220,7 @@ fn passes_gekkio_acceptance_timer_tma_write_reloading() {
         "../roms/gekkio/acceptance/timer/tma_write_reloading.gb"
     ))
     .must_run_and_match(
-        30_000_000u64,
+        4_000_000u64,
         include_bytes!("gekkio/acceptance/timer/tma_write_reloading.bin"),
     );
 }
