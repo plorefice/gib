4a4745c361d820d0f23fc04554ad283529b40f67
Pietro Lorefice <pietro.lorefice@gmail.com>
2019-02-19 22:22:30 +0100

    tests: add passing blargg's dmg_sound-2 tests #1 and #2

--- tests/blargg_golden.rs
+++ tests/blargg_golden.rs
@@ -25,3 +25,23 @@ fn passes_blargg_mem_timing_2() {
     RomTest::new(include_bytes!("../roms/blargg/mem_timing-2.gb"))
         .must_run_and_match(12_000_000u64, include_bytes!("blargg/mem_timing-2.bin"));
 }
+
+/*
+ * dmg_sound-2 single ROMs
+ */
+
+#[test]
+fn passes_blargg_dmg_sound_01_registers() {
+    RomTest::new(include_bytes!("../roms/blargg/dmg_sound-2/01-registers.gb")).must_run_and_match(
+        4_000_000u64,
+        include_bytes!("blargg/dmg_sound-2/01-registers.bin"),
+    )
+}
+
+#[test]
+fn passes_blargg_dmg_sound_02_len_ctl() {
+    RomTest::new(include_bytes!("../roms/blargg/dmg_sound-2/02-len ctr.gb")).must_run_and_match(
+        40_000_000u64,
+        include_bytes!("blargg/dmg_sound-2/02-len ctr.bin"),
+    )
+}
