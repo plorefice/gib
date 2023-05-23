93e8f9008847a98ca765247955237a5d77bf4ded
Pietro Lorefice <pietro@develer.com>
2023-05-23 18:47:25 +0200

    gib: replace imgui with egui for the UI

--- src/ui/views/peripherals.rs
+++ src/ui/views/peripherals.rs
@@ -1,149 +1,142 @@
-use imgui::{CollapsingHeader, Condition, Ui};
+use egui::Color32;
 
 use crate::ui::{state::EmuState, utils};
 
-use super::WindowView;
+#[derive(Default)]
+pub struct Peripherals;
 
-pub struct PeripheralView;
+impl super::Window for Peripherals {
+    fn name(&self) -> &'static str {
+        "Peripherals"
+    }
 
-impl PeripheralView {
-    pub fn new() -> PeripheralView {
-        PeripheralView
+    fn show(&mut self, ctx: &egui::Context, state: &mut EmuState, open: &mut bool) {
+        egui::Window::new(self.name())
+            .default_pos([915.0, 30.0])
+            .default_size([310.0, 650.0])
+            .open(open)
+            .show(ctx, |ui| {
+                use super::View;
+                self.ui(ui, state);
+            });
     }
 }
 
-impl WindowView for PeripheralView {
-    fn draw(&mut self, ui: &Ui, state: &mut EmuState) -> bool {
-        let mut open = true;
+impl super::View for Peripherals {
+    fn ui(&mut self, ui: &mut egui::Ui, state: &mut EmuState) {
+        egui::CollapsingHeader::new("Video Display").show(ui, |ui| {
+            ui.label("NOT IMPLEMENTED YET!");
+        });
 
-        ui.window("Peripherals")
-            .size([310.0, 650.0], Condition::FirstUseEver)
-            .position([955.0, 30.0], Condition::FirstUseEver)
-            .opened(&mut open)
-            .build(|| {
-                if CollapsingHeader::new("Video Display").build(ui) {
-                    ui.text("NOT IMPLEMENTED YET!");
-                }
-
-                if CollapsingHeader::new("Sound Controller")
-                    .default_open(true)
-                    .build(ui)
-                {
-                    self.draw_sound_controller(ui, state);
-                }
-
-                if CollapsingHeader::new("Joypad Input").build(ui) {
-                    ui.text("NOT IMPLEMENTED YET!");
-                }
-
-                if CollapsingHeader::new("Link Cable").build(ui) {
-                    ui.text("NOT IMPLEMENTED YET!");
-                }
-
-                if CollapsingHeader::new("Timer and Divider")
-                    .default_open(true)
-                    .build(ui)
-                {
-                    self.draw_timer(ui, state);
-                }
-
-                if CollapsingHeader::new("Interrupts")
-                    .default_open(true)
-                    .build(ui)
-                {
-                    self.draw_interrupts(ui, state);
-                }
+        egui::CollapsingHeader::new("Sound Controller")
+            .default_open(true)
+            .show(ui, |ui| {
+                self.sound_controller_ui(ui, state);
             });
 
-        open
+        egui::CollapsingHeader::new("Joypad Input").show(ui, |ui| {
+            ui.label("NOT IMPLEMENTED YET!");
+        });
+
+        egui::CollapsingHeader::new("Link Cable").show(ui, |ui| {
+            ui.label("NOT IMPLEMENTED YET!");
+        });
+
+        egui::CollapsingHeader::new("Timer and Divider")
+            .default_open(true)
+            .show(ui, |ui| {
+                self.timers_ui(ui, state);
+            });
+
+        egui::CollapsingHeader::new("Interrupts")
+            .default_open(true)
+            .show(ui, |ui| {
+                self.interrupts_ui(ui, state);
+            });
     }
 }
 
-impl PeripheralView {
-    fn draw_sound_controller(&self, ui: &Ui, state: &EmuState) {
+impl Peripherals {
+    fn sound_controller_ui(&self, ui: &mut egui::Ui, state: &EmuState) {
         let apu = &state.bus().apu;
 
-        // Sweep channel rendering
-        {
-            ui.text("Sweep Channel");
+        egui::Grid::new("sweep-channel")
+            .num_columns(2)
+            .min_col_width(150.)
+            .show(ui, |ui| {
+                ui.label("Sweep Channel");
 
-            ui.same_line_with_pos(150.0);
-            ui.text_colored(
-                if apu.ch1.enabled() {
-                    utils::GREEN
-                } else {
-                    utils::DARK_GREEN
-                },
-                "ENABLED",
-            );
+                ui.horizontal(|ui| {
+                    ui.colored_label(
+                        if apu.ch1.enabled() {
+                            Color32::GREEN
+                        } else {
+                            Color32::DARK_GREEN
+                        },
+                        "ENABLED",
+                    );
 
-            ui.same_line_with_pos(220.0);
-            ui.text_colored(
-                if apu.ch1.dac_on() {
-                    utils::GREEN
-                } else {
-                    utils::DARK_GREEN
-                },
-                "DAC",
-            );
+                    ui.add_space(30.);
 
-            ui.separator();
-        }
+                    ui.colored_label(
+                        if apu.ch1.dac_on() {
+                            Color32::GREEN
+                        } else {
+                            Color32::DARK_GREEN
+                        },
+                        "DAC",
+                    );
+                });
+            });
 
-        ui.spacing();
+        ui.separator();
 
-        // Tone channel rendering
-        {
-            ui.text("Tone Channel");
+        egui::Grid::new("tone-channel")
+            .num_columns(2)
+            .min_col_width(150.)
+            .show(ui, |ui| {
+                ui.label("Tone Channel");
 
-            ui.same_line_with_pos(150.0);
-            ui.text_colored(
-                if apu.ch1.enabled() {
-                    utils::GREEN
-                } else {
-                    utils::DARK_GREEN
-                },
-                "ENABLED",
-            );
+                ui.horizontal(|ui| {
+                    ui.colored_label(
+                        if apu.ch1.enabled() {
+                            Color32::GREEN
+                        } else {
+                            Color32::DARK_GREEN
+                        },
+                        "ENABLED",
+                    );
 
-            ui.same_line_with_pos(220.0);
-            ui.text_colored(
-                if apu.ch1.dac_on() {
-                    utils::GREEN
-                } else {
-                    utils::DARK_GREEN
-                },
-                "DAC",
-            );
+                    ui.add_space(30.);
 
-            ui.separator();
-        }
+                    ui.colored_label(
+                        if apu.ch1.dac_on() {
+                            Color32::GREEN
+                        } else {
+                            Color32::DARK_GREEN
+                        },
+                        "DAC",
+                    );
+                });
+            });
 
-        ui.spacing();
+        ui.separator();
 
-        // Wave channel rendering
-        {
-            ui.text("Wave Channel");
-            ui.separator();
-        }
+        ui.label("Wave Channel");
 
-        ui.spacing();
+        ui.separator();
 
-        // Noise channel rendering
-        {
-            ui.text("Noise Channel");
-            ui.separator();
-        }
+        ui.label("Noise Channel");
     }
 
-    fn draw_timer(&self, ui: &Ui, state: &EmuState) {
+    fn timers_ui(&self, ui: &mut egui::Ui, state: &EmuState) {
         let timer = &state.bus().tim;
 
-        utils::input_addr(ui, "DIV", &mut Some(timer.sys_counter.0), false);
-        ui.same_line();
-        utils::input_addr(ui, "TIMA", &mut Some(u16::from(timer.tima.0)), false);
-        ui.same_line();
-        utils::input_addr(ui, "TMA", &mut Some(u16::from(timer.tma.0)), false);
+        ui.horizontal(|ui| {
+            utils::address_edit_ui(ui, "DIV", &mut utils::hexify(timer.sys_counter.0), false);
+            utils::address_edit_ui(ui, "TIMA", &mut utils::hexify(timer.tima.0), false);
+            utils::address_edit_ui(ui, "TMA", &mut utils::hexify(timer.tma.0), false);
+        });
 
         ui.separator();
 
@@ -155,21 +148,21 @@ impl PeripheralView {
             _ => unreachable!(),
         };
 
-        ui.text(format!("Clock: {}", rate));
-
-        ui.same_line_with_spacing(0.0, 40.0);
-
-        ui.text_colored(
-            if (timer.tac.0 & 0x4) != 0 {
-                utils::GREEN
-            } else {
-                utils::DARK_GREEN
-            },
-            "RUNNING",
-        );
+        ui.horizontal(|ui| {
+            ui.label(format!("Clock: {rate}"));
+            ui.add_space(40.0);
+            ui.colored_label(
+                if (timer.tac.0 & 0x4) != 0 {
+                    Color32::GREEN
+                } else {
+                    Color32::DARK_GREEN
+                },
+                "RUNNING",
+            );
+        });
     }
 
-    fn draw_interrupts(&self, ui: &Ui, state: &EmuState) {
+    fn interrupts_ui(&self, ui: &mut egui::Ui, state: &EmuState) {
         let itr = &state.bus().itr;
         let irqs = [
             (0, "BLANK"),
@@ -179,32 +172,36 @@ impl PeripheralView {
             (4, "JOY"),
         ];
 
-        ui.text("IE:");
+        ui.horizontal(|ui| {
+            ui.label("IE:");
 
-        for (b, s) in irqs.iter() {
-            ui.same_line_with_spacing(0.0, 15.0);
-            ui.text_colored(
-                if itr.ien.bit(*b) {
-                    utils::GREEN
-                } else {
-                    utils::DARK_GREEN
-                },
-                s,
-            );
-        }
+            for &(b, s) in irqs.iter() {
+                ui.add_space(15.0);
+                ui.colored_label(
+                    if itr.ien.bit(b) {
+                        Color32::GREEN
+                    } else {
+                        Color32::DARK_GREEN
+                    },
+                    s,
+                );
+            }
+        });
 
-        ui.text("IF:");
+        ui.horizontal(|ui| {
+            ui.label("IF:");
 
-        for (b, s) in irqs.iter() {
-            ui.same_line_with_spacing(0.0, 15.0);
-            ui.text_colored(
-                if itr.ifg.bit(*b) {
-                    utils::GREEN
-                } else {
-                    utils::DARK_GREEN
-                },
-                s,
-            );
-        }
+            for &(b, s) in irqs.iter() {
+                ui.add_space(15.0);
+                ui.colored_label(
+                    if itr.ifg.bit(b) {
+                        Color32::GREEN
+                    } else {
+                        Color32::DARK_GREEN
+                    },
+                    s,
+                );
+            }
+        });
     }
 }
